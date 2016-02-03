1bec390b7cfad48d7bc0ae047558ed0e688307ca
Jan Bogaerts <jb@allthingstalk.com>
2016-02-03 13:37:27 +0100

    move libs to libraries folder

--- ATT_IOT_UART/examples/line_finder/keys.h
+++
@@ -1,8 +0,0 @@
-#ifndef SETTINGS
-#define SETTINGS
-
-#define DEVICEID "q8ib32QO2Qe29Ml4hQtwZXY"                                      // Your device id comes here
-#define CLIENTID "testjan"                                   // Your client id comes here;
-#define CLIENTKEY "5i4duakv2bq"                                 // Your client key comes here;
-
-#endif
--- ATT_IOT_UART/examples/relay/keys.h
+++
@@ -1,8 +0,0 @@
-
-// Enter below your client credentials. 
-//These credentials can be found in the configuration pane under your device in the smartliving.io website 
-
-#define DEVICEID "z0dVVZVvzWL4AyoOJTZ17ZW"                   // Your device id comes here
-#define CLIENTID "testjan"                                   // Your client id comes here;
-#define CLIENTKEY "5i4duakv2bq"                              // Your client key comes here;
-
