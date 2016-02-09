e8f40150c986219ff18407fa154cdf93cc963512
esthoril <shendrickx@hotmail.com>
2016-02-09 15:13:10 +0100

    Add Temperature Humidity sample sketch and library

---
+++ libraries/ATT_IOT_UART/examples/temperature_humidity_sensor/temperature_humidity_sensor.ino
@@ -0,0 +1,122 @@
+/*
+
+Arduino UART Demo Sketch. This Sketch is made for an Genuino 101 IoT board with a Grove
+UART WiFi module based on the popular ESP8266 IoT SoC to communicate to the AllThingsTalk
+IoT developer cloud.
+The Grove UART WiFi module has firmware installed which includes the ATT_IOT library. It
+communicates through Serial1 of the Genuino 101 board.
+
+Version 1.0 dd 26/12/2015
+
+This sketch is an example sketch to deploy the Grove Temperature Humidity (101020019) to the
+AllThingsTalk IoT developer cloud.
+
+
+### Instructions
+1. Setup the Arduino hardware
+  - Use an Arduino Genuino 101 IoT board
+  - Connect the Arduino Grove shield, make sure the switch is set to nullV
+  - Connect USB cable to your computer
+  - Connect a Grove Temperature Humidity to pin D7 of the Arduino shield
+  - Grove UART wifi to pin UART (D0,D1)
+2. Add 'ATT_IOT_UART' library to your Arduino Environment
+     More info can be found at http://arduino.cc/en/Guide/Libraries
+3. Fill in the missing strings (deviceId, clientId and clientKey) in the keys.h file
+4. Optionally, change sensor names, labels as appropiate
+5. Upload the sketch
+
+Note: for use of extra actuators, extend the callback function at the end of the sketch
+
+*/
+
+#include "ATT_IOT_UART.h"      // AllThingsTalk Arduino UART IoT library
+#include <SPI.h>               // Required to have support for signed/unsigned long type
+#include "keys.h"              // Keep all your personal account information in a seperate file
+#include "DHT.h"
+
+ATTDevice Device(&Serial1);
+char httpServer[] = "api.smartliving.io";          // HTTP API Server host
+char mqttServer[] = "broker.smartliving.io";       // MQTT Server Address
+
+// Define PIN numbers for assets
+// For digital and analog sensors, we recommend to use the physical pin id as the asset id
+// For other sensors (I2C and UART), you can select any unique number as the asset id
+#define sensorId 7                // Digital sensor is connected to pin D7 on grove shield
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+DHT dht(sensorId, DHTTYPE);
+
+// Required for the device
+void callback(int pin, String& value);
+
+void setup()
+{
+  Serial.begin(57600);                                 // Init serial link for debugging
+  while(!Serial && millis() < 1000);                   // Make sure you see all output on the monitor. After 1 sec, it will skip this step, so that the board can also work without being connected to a pc  Serial.println("Starting sketch");
+  Serial1.begin(115200);                               // Init serial link for WiFi module
+  while(!Serial1);
+
+  while(!Device.StartWifi())
+    Serial.println("Retrying...");
+  while(!Device.Init(DEVICEID, CLIENTID, CLIENTKEY))   // If we can't succeed to initialize and set the device credentials, there is no point to continue
+    Serial.println("Retrying...");
+  while(!Device.Connect(httpServer))                   // Connect the device with the AllThingsTalk IOT developer cloud. No point to continue if we can't succeed at this
+    Serial.println("Retrying");
+
+  Device.AddAsset(sensorId, "Temperature Humidity", "Values", false, "{\"type\": \"object\",\"properties\": {\"temperature (°C)\": {\"type\": \"number\"},\"humidity (%)\": {\"type\": \"number\"}}}");   // Create the sensor asset for your device
+
+  delay(1000);                                         // Give the WiFi some time to finish everything
+  while(!Device.Subscribe(mqttServer, callback))       // Make sure that we can receive message from the AllThingsTalk IOT developer cloud (MQTT). This stops the http connection
+    Serial.println("Retrying");
+
+  dht.begin();
+  pinMode(sensorId, INPUT);                       // Initialize the digital pin as an input.
+  Serial.println("Temperature Humidity is ready for use!");
+}
+
+void loop()
+{
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again)
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  String data;
+  data = "{\"temperature (°C)\":" + String(t, 2) + ",\"humidity(%)\":" + String(h, 2) + "}";
+  Device.Send(data, sensorId);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+
+  Device.Process();
+}
+
+
+// Callback function: handles messages that were sent from the iot platform to this device.
+void callback(int pin, String& value) 
+{ 
+  Serial.print("Incomming data for: ");        // Display the value that arrived from the AllThingsTalk IOT developer cloud.
+  Serial.print(pin);
+  Serial.print(", value: ");
+  Serial.println(value);
+  Device.Send(value, pin);                     // Send the value back for confirmation   
+}
+
