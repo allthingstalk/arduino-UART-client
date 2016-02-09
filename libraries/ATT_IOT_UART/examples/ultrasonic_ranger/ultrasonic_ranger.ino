3e56b3d487f9bd871ac06c99d326f707ded55a69
esthoril <shendrickx@hotmail.com>
2016-02-09 14:11:30 +0100

    Update ultrasonic ranger sample sketch

--- libraries/ATT_IOT_UART/examples/ultrasonic_ranger/ultrasonic_ranger.ino
+++ libraries/ATT_IOT_UART/examples/ultrasonic_ranger/ultrasonic_ranger.ino
@@ -1,72 +1,74 @@
 /*
-  Arduino UART Demo Sketch. This Sketch is made for an Genuino 101 IoT board with a Grove UART WiFi module 
-  based on the popular ESP8266 IoT SoC to communicate to the AllThingsTalk IoT developer cloud
 
-  The Grove UART WiFi module has a firmware installed which includes the ATT_IOT library. The UART WiFi module communicates through Serial1 of the genuino 101 board.
-  
-  version 1.0 dd 26/12/2015
-  
-  This sketch is an example sketch to deploy the Ultrasonic Ranger (101020010) to the AllThingsTalk IoT developer cloud. 
- 
-  
-  ### Instructions
+Arduino UART Demo Sketch. This Sketch is made for an Genuino 101 IoT board with a Grove
+UART WiFi module based on the popular ESP8266 IoT SoC to communicate to the AllThingsTalk
+IoT developer cloud.
+The Grove UART WiFi module has firmware installed which includes the ATT_IOT library. It
+communicates through Serial1 of the Genuino 101 board.
 
-  1. Setup the Arduino hardware
-    - Use an Arduino Genuino 101 IoT board
-    - Connect USB cable to your computer
-    - Connect the Arduino Grove shield
-    - connect the Ultrasonic Ranger sensor to PIN D7 of the Arduino shield
-    - Grove UART wifi to pin UART (D0,D1)
+Version 1.0 dd 26/12/2015
 
-  2. Add 'ATT_IOT_UART' library to your Arduino Environment. [Try this guide](http://arduino.cc/en/Guide/Libraries)
-  3. fill in the missing strings (deviceId, clientId, clientKey, WIFI_SSID,WIFI_PWD) in the settings.h file. 
-  4. Optionally, change sensor names, labels as appropiate. For extra actuators, make certain to extend the callback code at the end of the sketch.
-  4. Upload the sketch
+This sketch is an example sketch to deploy the Grove Ultrasonic ranger (101020010) to the
+AllThingsTalk IoT developer cloud.
+
+
+### Instructions
+1. Setup the Arduino hardware
+  - Use an Arduino Genuino 101 IoT board
+  - Connect the Arduino Grove shield
+  - Connect USB cable to your computer
+  - Connect a Grove Ultrasonic ranger to pin D7 of the Arduino shield
+  - Grove UART wifi to pin UART (D0,D1)
+2. Add 'ATT_IOT_UART' library to your Arduino Environment
+     More info can be found at http://arduino.cc/en/Guide/Libraries
+3. Fill in the missing strings (deviceId, clientId and clientKey) in the keys.h file
+4. Optionally, change sensor names, labels as appropiate
+5. Upload the sketch
+
+Note: for use of extra actuators, extend the callback function at the end of the sketch
 
 */
 
-#include "ATT_IOT_UART.h"                       //AllThingsTalk Arduino UART IoT library
-#include <SPI.h>                                //required to have support for signed/unsigned long type.
-#include "keys.h"                           	//keep all your personal account information in a seperate file
+#include "ATT_IOT_UART.h"      // AllThingsTalk Arduino UART IoT library
+#include <SPI.h>               // Required to have support for signed/unsigned long type
+#include "keys.h"              // Keep all your personal account information in a seperate file
 #include "Ultrasonic.h"
 
-
-ATTDevice Device(&Serial1);                  
-char httpServer[] = "api.smartliving.io";                       // HTTP API Server host                  
-char mqttServer[] = "broker.smartliving.io";                    // MQTT Server Address
-
+ATTDevice Device(&Serial1);
+char httpServer[] = "api.smartliving.io";          // HTTP API Server host
+char mqttServer[] = "broker.smartliving.io";       // MQTT Server Address
 
 // Define PIN numbers for assets
-#define ultrasonicPin 7                                        // Ultrasonic ranger Sensor is connected to pin D7 on grove shield 
-Ultrasonic ultrasonic(ultrasonicPin);
+// For digital and analog sensors, we recommend to use the physical pin id as the asset id
+// For other sensors (I2C and UART), you can select any unique number as the asset id
+#define distanceId 7                // Digital sensor is connected to pin D7 on grove shield
+Ultrasonic ultrasonic(distanceId);
 
-//required for the device
+// Required for the device
 void callback(int pin, String& value);
 
-
 void setup()
-{          
-
-  Serial.begin(57600);                                         // init serial link for debugging
-  
-  while (!Serial) ;                                            // This line makes sure you see all output on the monitor. REMOVE THIS LINE if you want your IoT board to run without monitor !
-  Serial.println("Starting sketch");
-  Serial1.begin(115200);                                       //init serial link for wifi module
+{
+  Serial.begin(57600);                                 // Init serial link for debugging
+  while(!Serial && millis() < 1000);                   // Make sure you see all output on the monitor. After 1 sec, it will skip this step, so that the board can also work without being connected to a pc  Serial.println("Starting sketch");
+  Serial1.begin(115200);                               // Init serial link for WiFi module
   while(!Serial1);
-  
+
   while(!Device.StartWifi())
-    Serial.println("retrying...");
-  while(!Device.Init(DEVICEID, CLIENTID, CLIENTKEY))           //if we can't succeed to initialize and set the device credentials, there is no point to continue
-    Serial.println("retrying...");
-  while(!Device.Connect(httpServer))                           // connect the device with the AllThingsTalk IOT developer cloud. No point to continue if we can't succeed at this
-    Serial.println("retrying");
-    
-  Device.AddAsset(ultrasonicPin, "Ultrasonic sensor", "Ultrasonic Ranger sensor", false, "{\"type\": \"number\", \"minimum\": 0, \"unit\": \"cm\"}");   // Create the Sensor asset for your device
-  
-  delay(1000);                                                 //give the wifi some time to finish everything
-  while(!Device.Subscribe(mqttServer, callback))               // make sure that we can receive message from the AllThingsTalk IOT developer cloud  (MQTT). This stops the http connection
-	Serial.println("retrying");
-   
+    Serial.println("Retrying...");
+  while(!Device.Init(DEVICEID, CLIENTID, CLIENTKEY))   // If we can't succeed to initialize and set the device credentials, there is no point to continue
+    Serial.println("Retrying...");
+  while(!Device.Connect(httpServer))                   // Connect the device with the AllThingsTalk IOT developer cloud. No point to continue if we can't succeed at this
+    Serial.println("Retrying");
+
+  Device.AddAsset(distanceId, "Ultrasonic ranger", "distance", false, "{\"type\": \"number\", \"minimum\": 0, \"unit\": \"cm\"}");   // Create the sensor asset for your device
+
+  delay(1000);                                         // Give the WiFi some time to finish everything
+  while(!Device.Subscribe(mqttServer, callback))       // Make sure that we can receive message from the AllThingsTalk IOT developer cloud (MQTT). This stops the http connection
+    Serial.println("Retrying");
+
+  pinMode(distanceId, INPUT);                       // Initialize the digital pin as an input.
+  Serial.println("Ultrasonic ranger is ready for use!");
 }
 
 long prev_Value = 0;
@@ -77,7 +79,7 @@ void loop()
   if (RangeInCentimeters != prev_Value) {                            // verify if value has changed
     Serial.print("RangeInCentimeters = ");
     Serial.println(RangeInCentimeters);
-    Device.Send(String(RangeInCentimeters), ultrasonicPin);
+    Device.Send(String(RangeInCentimeters), distanceId);
     prev_Value = RangeInCentimeters;
     }
   Device.Process(); 
